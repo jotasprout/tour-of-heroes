ce16c4f19f90a1bb83e520d9250be2c0285ad9a4
Jay Sprout <jotasprout@gmail.com>
2022-08-15 05:07:00 -0400

    practicing angular

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 12, name: 'Dr. Nice' },
+    {id: 13, name: 'Monstro' },
+    {id: 14, name: 'Dr. Dumbass' },
+    {id: 15, name: 'Celery Man' },
+    {id: 16, name: 'Booger Eater' },
+    {id: 17, name: 'Purple Cloak' },
+    {id: 18, name: 'Cowboy Dog' },
+    {id: 19, name: 'Dirty Dog' },
+    {id: 20, name: 'Kitten Woman' }
+];
